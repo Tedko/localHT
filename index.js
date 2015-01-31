975abd2e9e88c9558d0e8596b6ac9e62e3f0aa55
Belleve Invis <belleve@typeof.net>
2015-02-01 04:25:23 +0800

    Switched into adm-zip due to node-zipfile does not support Unicode path on windows

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-var express	= require('express')
-var zipfile	= require('zipfile')
-var fs     	= require('fs')
-var path   	= require('path')
-var alphanumSort = require('./alphanum').alphanumSort;
+var express     	= require('express')
+var AdmZip      	= require('adm-zip')
+var fs          	= require('fs')
+var path        	= require('path')
+var alphanumSort	= require('./alphanum').alphanumSort;
+var argv        	= require('yargs').argv;
 
 var app = express();
 var ziplist = null;
 
+var config = {
+	archive : argv.archive
+}
+
 function getZipList(callback){
 	if(ziplist) return callback(ziplist);
-	var directory = 'D:\\LHTArchive'
+	var directory = config.archive;
 	fs.readdir(directory, function(err, contents){
-		if(err) return res.status(404).send('LHT Archive not accessable.');
-		contents = contents.filter(function(file){ return /\.zip$/.test(file) && fs.statSync(path.join(directory, file)).isFile()}).sort().map(function(fileName){
+		if(err) return callback(ziplist = []);
+		contents = contents.sort().map(function(fileName){
+			if(!/\.zip$/.test(fileName)) return null;
+			var stats = fs.statSync(path.join(directory, fileName))
+			if(!stats.isFile()) return null;
 			return {
-				fileName : fileName,
-				fullName : path.join(directory, fileName)
+				fileName: fileName,
+				fullName: path.join(directory, fileName),
+				mdate: stats.mdate
 			}
-		});
+		}).filter(function(x){ return !!x });
 		ziplist = contents;
 		return callback(ziplist);
 	})
@@ -25,28 +34,34 @@ function getZipList(callback){
 
 app.get('/image/:bid/:page', function (req, res) {
 	getZipList(function(zl){
-		var zf = new zipfile.ZipFile(zl[req.params.bid].fullName);
-		zf.readFile(req.params.page, function(err, buffer){
-			if(!err){
-				res.write(buffer);
-				res.end();
-			} else {
-				res.status(404).send('Image not found.');
+		var zf = new AdmZip(zl[req.params.bid].fullName).getEntries();
+		for(var j = 0; j < zf.length; j++){
+			if(zf[j].entryName === req.params.page) {
+				zf[j].getDataAsync(function(buf){
+					res.status(200).write(buf);
+					res.end();					
+				});
+				return
 			}
-		})
+		}
+		res.status(404).send('Image not found.');
 	})
 });
 
 app.get('/pages/:bid', function (req, res) {
 	getZipList(function(zl){
-		var zf = new zipfile.ZipFile(zl[req.params.bid].fullName);
-		res.status(200).set('Content-Type', 'text/html');
-		res.write('<ol>' + alphanumSort(zf.names).map(function(x){ return '<li><img src="/image/' + req.params.bid + '/' + x + '" width=100 height=100/></li>' }).join('\n') + '</ol>');
-		res.end();
+		try {
+			var zf = new AdmZip(zl[req.params.bid].fullName).getEntries().map(function(entry){ return entry.entryName });
+			res.status(200).set('Content-Type', 'text/html');
+			res.write('<ol>' + alphanumSort(zf).map(function(x){ return '<li><img src="/image/' + req.params.bid + '/' + x + '" width=100 height=100/></li>' }).join('\n') + '</ol>');
+			res.end();
+		} catch(e){
+			res.status(500).send(e + '').end();
+		}
 	})
 });
 
-app.get('/all', function (req, res) {
+app.get('/', function (req, res) {
 	getZipList(function(zl){
 		res.set('Content-Type', 'text/html');
 		res.send('<ol>' + zl.map(function(entry, bid){ return '<li><a href="/pages/' + bid + '">' + entry.fileName + '</a></li>'}).join('\n') + '</ol>')
@@ -56,5 +71,6 @@ app.get('/all', function (req, res) {
 var server = app.listen(3000, function () {
 	var host = server.address().address
 	var port = server.address().port
-	console.log('LocalHT listening at http://%s:%s', host, port)
+	console.log('[init] LocalHT listening at http://%s:%s', host, port);
+	console.log('[init] LocalHT Archive is %s', config.archive);
 })
